2afefe286424092ec7b996a3e84e55f03f7dd043
ernfrncz <126535224+ernfrncz@users.noreply.github.com>
2023-03-28 11:12:31 +0800

    Update for.js

--- for.js
+++ for.js
@@ -1,6 +1,6 @@
 for(let i=1, z=1; i<=4108; i=i*2+z, z=z-1){
-    document.writeln(parseInt(i));
+    document.write(parseInt(i));
     if(i<4108){
-        document.writeln(",");
+        document.write(", ");
     }
 }
